7ced2d6cb148725bddf163f9616b623f3e572b3b
albert <albert@alberts-MacBook-Pro.local>
2025-11-08 14:55:49 +0100

    gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { Analytics } from '@vercel/analytics/react'
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: {
@@ -22,6 +23,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-H2PC2S43VZ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-H2PC2S43VZ');
+          `}
+        </Script>
         {children}
         <Analytics />
       </body>
